94133beae2d80ee62deded8697ca0af3e58c50dd
tsbischof <tsbischof>
2010-06-05 07:19:42 +0000

    *** empty log message ***

---
+++ src/systems/trig3.h
@@ -0,0 +1,10 @@
+#ifndef TRIG3_H_
+#define TRIG3_H_
+
+#include "includes.h"
+
+double force_trig3(double *r);
+void integrate_trig3(double *r, double dt);
+void do_trig3(dictionary *options, Grapher *grapher);
+
+#endif
