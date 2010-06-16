74aca51ed58ad9876cb18e50e60f6488c4e728b0
tsbischof <tsbischof>
2010-06-16 21:50:20 +0000

    *** empty log message ***

--- run/lots_of_runs.py
+++ run/lots_of_runs.py
@@ -1,10 +1,10 @@
-input_file = """integrator = double_pendulum
+input_file = """system = double_pendulum
 rule = first_flip
-validator = energy_double_pendulum
+validator = energy
 
 t = 0,0.01,100
-phi1 = -3.14,0.0314,3.14
-phi2 = -3.14,0.0314,3.14
+phi1 = -3.14159,0.01256,3.14159
+phi2 = -3.14159,0.01256,3.14159
 omega1 = %f
 omega2 = %f
 plot = phi1,phi2
@@ -16,13 +16,13 @@ omega2_init = -3
 domega1 = 0.1
 domega2 = 0.1
 
-omega1_limit = 3
-omega2_limit = 3
+omega1_limit = 3.01
+omega2_limit = 3.01
 
 n = 0
-while omega1 < omega1_limit:
+while omega1 <= omega1_limit:
     omega2 = omega2_init
-    while omega2 < omega2_limit:
+    while omega2 <= omega2_limit:
         f = open("dp%08d.inp" % n, "w")
         f.write(input_file % (omega1, omega2))
         f.close()
--- run/runall.py
+++ run/runall.py
@@ -7,19 +7,22 @@ import sys
 import os
 
 def do_run(filename):
-    lagrangians = "/home/tsbischof/src/lagrangians/src/lagrangians"
+    lagrangians = "/home/tsbischof/src/lagrangians/lagrangians"
     base = filename[:-3]
+    if os.path.isfile(base+"png"):
+        return()
     log_name = base + "log"
     print("Working with input file", filename)
     log_file = open(log_name, "w")
     subprocess.Popen([lagrangians, filename], stdout=log_file).wait()
     subprocess.Popen(["convert", base + "ppm", base + "png"]).wait()
     subprocess.Popen(["bzip2", base + "raw"]).wait()
+    subprocess.Popen(["convert", base + "ppm", base + "png"]).wait()
     subprocess.Popen(["bzip2", base + "ppm"]).wait()
     log_file.close()
 
 if __name__ == "__main__":
-    pool = Pool()
+    pool = Pool(1)
 
     input_files = list()
     for filename in filter(lambda x: x.endswith(".inp"), sys.argv):
